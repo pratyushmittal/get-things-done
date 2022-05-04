127d805ad1a51ec053168465c0d55af4b6be184d
Pratyush Mittal <pratyushmittal@gmail.com>
2022-05-04 11:37:21 +0530

    Added keyboard shortcut for active

---
+++ www/js/app.js
@@ -0,0 +1,34 @@
+(function () {
+  function getAllTaskIds () {
+    var allTaskIds = []
+    document.querySelectorAll('[data-task-id]').forEach(el => {
+      allTaskIds.push(el.dataset.taskId)
+    })
+    return allTaskIds
+  }
+
+  function getNextTaskId (activeTaskId) {
+    var allTaskIds = getAllTaskIds()
+    var targetIdx = allTaskIds.indexOf(activeTaskId) + 1
+    if (targetIdx >= allTaskIds.length) targetIdx = allTaskIds.length - 1
+    return allTaskIds[targetIdx]
+  }
+
+  function getPreviousTaskId (activeTaskId) {
+    var allTaskIds = getAllTaskIds()
+    var targetIdx = allTaskIds.indexOf(activeTaskId) - 1
+    if (targetIdx < 0) targetIdx = 0
+    return allTaskIds[targetIdx]
+  }
+
+  function setupEverything () {
+    document.addEventListener('keyup', function (e) {
+      console.log(e.currentTarget)
+    })
+    window.App = {
+      getNextTaskId: getNextTaskId,
+      getPreviousTaskId: getPreviousTaskId
+    }
+  }
+  setupEverything()
+})()
